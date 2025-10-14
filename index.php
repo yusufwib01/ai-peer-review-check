3e68bfc16d4aed974f247fbce14dc83a5ac5cdd1
yusufwib01 <yusuf.wibisono@moodle.com>
2025-10-14 16:42:09 +0700

    test

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
 
-echo "Hello, World!";
+$users = [
+    ['id' => 1, 'name' => 'Alice'],
+    ['id' => 2, 'name' => 'Bob'],
+    ['id' => 3, 'name' => 'Charlie'],
+];
+
+$posts = [
+    ['user_id' => 1, 'title' => 'Post A'],
+    ['user_id' => 2, 'title' => 'Post B'],
+    ['user_id' => 1, 'title' => 'Post C'],
+    ['user_id' => 3, 'title' => 'Post D'],
+];
+
+foreach ($users as $user) {
+    $userposts = [];
+    foreach ($posts as $post) {
+        if ($post['user_id'] === $user['id']) {
+            $userposts[] = $post;
+        }
+    }
+    echo $user['name'] . ' has ' . count($userposts) . " posts\n";
+}
